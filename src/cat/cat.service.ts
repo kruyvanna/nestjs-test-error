ed21914f4b7ebe77f1b34756ed48bb4d195d6af5
Kruy Vanna <kruyvanna@gmail.com>
2022-04-14 16:16:49 +0700

    inject Model into Service

--- src/cat/cat.service.ts
+++ src/cat/cat.service.ts
@@ -1,4 +1,18 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Cat, CatDocument } from './schemas/cat.schema';
 
 @Injectable()
-export class CatService {}
+export class CatsService {
+  constructor(@InjectModel(Cat.name) private catModel: Model<CatDocument>) {}
+
+  async create(data: Cat): Promise<Cat> {
+    const createdCat = new this.catModel(data);
+    return createdCat.save();
+  }
+
+  async findAll(): Promise<Cat[]> {
+    return this.catModel.find().exec();
+  }
+}
--- src/owner/owner.module.ts
+++ src/owner/owner.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { OwnerController } from './owner.controller';
+import { OwnerService } from './owner.service';
 
 @Module({
-  controllers: [OwnerController]
+  controllers: [OwnerController],
+  providers: [OwnerService]
 })
 export class OwnerModule {}
