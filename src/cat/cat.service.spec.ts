6fd7f537366995e88501134d4c89f676e92bb4ae
Kruy Vanna <kruyvanna@gmail.com>
2022-04-14 19:33:23 +0700

    [Fixed] can run test with service that use Mongoose

--- src/cat/cat.service.spec.ts
+++ src/cat/cat.service.spec.ts
@@ -1,12 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { Cat } from './schemas/cat.schema';
 import { CatService } from './cat.service';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('CatService', () => {
   let service: CatService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CatService],
+      providers: [
+        CatService,
+        { provide: getModelToken(Cat.name), useValue: jest.fn() },
+      ],
     }).compile();
 
     service = module.get<CatService>(CatService);
