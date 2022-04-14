b6e96794a8b5f17a207696a293011fcade378cef
Kruy Vanna <kruyvanna@gmail.com>
2022-04-14 15:36:39 +0700

    add cat module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { CatModule } from './cat/cat.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, MongooseModule.forRoot('mongodb://localhost/nest'), CatModule],
   controllers: [AppController],
   providers: [AppService],
 })
