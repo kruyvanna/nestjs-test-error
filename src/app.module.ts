b10f975f881d402eef29e6f29df3218095751515
Kruy Vanna <kruyvanna@gmail.com>
2022-04-14 17:08:29 +0700

    add modules to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatModule } from './cat/cat.module';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './user/user.module';
-import { CatModule } from './cat/cat.module';
 import { OwnerModule } from './owner/owner.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, MongooseModule.forRoot('mongodb://localhost/nest'), CatModule, OwnerModule],
+  imports: [
+    UserModule,
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    CatModule,
+    OwnerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
